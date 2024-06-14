816f89d7e4eabbcc98beb4eba9deab953b02108d
grighq <ag@grighq.ru>
2024-06-14 08:49:21 +0300

    move roman func to main

--- main.go
+++ main.go
@@ -31,13 +31,6 @@ func main() {
   }
 }
 
-func getValidNum(val string) int {
-  if num, err := strconv.Atoi(val); num > 0 && num <= 10 && err == nil {
-    return num
-  }
-  panic("Ошибка ввода! Введите только римские или только арабские числа от 1 до 10")
-}
-
 func calculate(num1, num2 int, sign string) int {
   switch sign {
   case "+":
@@ -51,4 +44,42 @@ func calculate(num1, num2 int, sign string) int {
   default:
     panic("Ошибка ввода! Доступны только операции +,-,*,/")
   }
+}
+
+func getValidNum(val string) int {
+  if num, err := strconv.Atoi(val); num > 0 && num <= 10 && err == nil {
+    return num
+  }
+  panic("Ошибка ввода! Введите только римские или только арабские числа от 1 до 10")
+}
+
+func getValidRomanNums(val1, val2 string) (int, int, bool) {
+  validRomanNums := map[string]int{
+    "I":   1,
+    "II":  2,
+    "III": 3,
+    "IV":  4,
+    "V":   5,
+    "VI":  6,
+    "VII": 7,
+    "IIX": 8,
+    "IX":  9,
+    "X":   10,
+  }
+  num1, ok1 := validRomanNums[val1]
+  num2, ok2 := validRomanNums[val2]
+  return num1, num2, ok1 && ok2
+}
+
+func intToRoman(num int) string {
+  intVals := []int{100, 90, 50, 40, 10, 9, 5, 4, 1}
+  romanVals := []string{"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
+  romanNum := ""
+  for i, val := range intVals {
+    for num >= val {
+      romanNum += romanVals[i]
+      num -= val
+    }
+  }
+  return romanNum
 }
